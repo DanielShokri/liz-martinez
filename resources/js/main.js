0ae2e498ffa3087c4801eb5dd6f72c27e5cd59fe
Daniel Shokri <danielshokri@gmail.com>
2020-10-24 18:37:27 +0300

    first commit

---
+++ resources/js/main.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+  $("#owl-demo")
+    .on("initialized.owl.carousel changed.owl.carousel", function (e) {
+      if (!e.namespace) {
+        return;
+      }
+      const carousel = e.relatedTarget;
+      const counter = carousel.relative(carousel.current()) + 1;
+      $(".slider-counter").text(
+        "0" + counter + "/" + 0 + carousel.items().length
+      );
+    })
+    .owlCarousel({
+      navigation: false, // Show next and prev buttons
+      loop: true,
+      slideSpeed: 500,
+      paginationSpeed: 600,
+      center: false,
+      items: 1,
+      stagePadding: 0,
+      responsiveClass: true,
+      responsive: {
+        375: {
+          items: 1,
+          nav: false,
+          // autoHeight: true,
+        },
+      },
+    });
+});
+
+// Modal
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
