60b2f0ad782f00b58ba99048ae2a77013bbd3909
Daniel Shokri <danielshokri@gmail.com>
2020-10-25 14:46:15 +0200

    make bugs fixes on background image and 1440px res

--- dist/resources/js/clickOnBackground.js
+++ dist/resources/js/clickOnBackground.js
@@ -1,4 +1,4 @@
 const mainSite = document.querySelector(".full-width-click");
 mainSite.addEventListener("click", () => {
-  window.location.replace("catalog/model1/lilydress.html");
+  window.location.href = "catalog/model1/lilydress.html";
 });
