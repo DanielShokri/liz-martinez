250f7b51b209f169aa87e5ffe5b8be09ed6cb0d2
Daniel Shokri <danielshokri@gmail.com>
2020-10-30 18:11:20 +0200

    portectedRoute

--- dist/resources/js/clickOnBackground.js
+++ dist/resources/js/clickOnBackground.js
@@ -1,8 +1,24 @@
-const mainSite = document.querySelector(".full-width-click");
-if (mainSite) {
-  mainSite.addEventListener("click", () => {
+// const mainSite = document.querySelector(".full-width-click");
+// if (mainSite) {
+//   mainSite.addEventListener("click", () => {
+//     window.location.href = "catalog/model1/lilydress.html";
+//   });
+// }
+
+function checkPassword() {
+  var password = document.getElementById("password").value;
+  //console.log("users value is: " + password);
+  if (password == "LoveLiz2021") {
     window.location.href = "catalog/model1/lilydress.html";
-  });
+  } else {
+    document.getElementById("error").style.display = "inline-block";
+  }
+}
+
+function search(ele) {
+  if (event.key === "Enter") {
+    checkPassword();
+  }
 }
 
 const firstPoster = document.querySelector(".full-width-click-poster-1");
